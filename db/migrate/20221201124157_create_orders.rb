f38251447bcd762ce9072b8007e22f3594738a19
donrow007 <wn.nw106@gmail.com>
2022-12-01 21:44:29 +0900

    ordersテーブルを作成しましょう

--- db/migrate/20221201124157_create_orders.rb
+++ db/migrate/20221201124157_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.integer :price  ,null: false
+      t.timestamps
       t.timestamps
     end
   end
